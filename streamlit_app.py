f2631d947f9ba9930c2e000208ee3dc01630188b
Siarhei Damenikan <siarhei_damenikan@epam.com>
2024-12-04 15:24:12 +0100

    added pd

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col("FRUI
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+#st.dataframe(pd_df)
+#st.stop
 
 ing_list = st.multiselect('select fruit', my_dataframe, max_selections = 5)
 
@@ -37,6 +37,10 @@ if ing_list:
 
     for each_ing in ing_list:
         ing_string += each_ing + ' '
+        
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(each_ing + ' Nutrition Info')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+each_ing)
         st.text(smoothiefroot_response.json())
