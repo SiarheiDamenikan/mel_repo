f2c830b878559b452287628a0b088335e14071d9
Siarhei Damenikan <siarhei_damenikan@epam.com>
2024-12-04 15:10:02 +0100

    adeed request/response for each fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ if ing_list:
 
     for each_ing in ing_list:
         ing_string += each_ing + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(each_ing + ' Nutrition Info')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+each_ing)
         st.text(smoothiefroot_response.json())
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     if ing_string:
